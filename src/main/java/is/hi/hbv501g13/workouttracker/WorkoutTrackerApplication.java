4adaa470a226468f5a25e1232a8dcf7b69d9dec0
telmajohanns <tej2@hi.is>
2022-08-31 18:51:35 +0000

    Initial commit

---
+++ src/main/java/is/hi/hbv501g13/workouttracker/WorkoutTrackerApplication.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g13.workouttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/is/hi/hbv501g13/workouttracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g13.workouttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
