cd051cb4fad5f58d54cb25c4bad6b8a244abe955
viktoriaemma <43034699+viktoriaemma@users.noreply.github.com>
2022-09-01 09:58:21 +0000

    Fyrsta commit-ið hennar Viktoríu

--- src/main/java/is/hi/hbv501g13/workouttracker/WorkoutTrackerApplication.java
+++ src/main/java/is/hi/hbv501g13/workouttracker/WorkoutTrackerApplication.java
@@ -1,5 +1,6 @@
 package is.hi.hbv501g13.workouttracker;
 // This is team member Steinunn María Egilsdóttir
+// This is team member Viktoría Emma Berglindardóttir
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
