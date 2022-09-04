dc8f6579ee7d37a23843091bd64f3504290ddab5
steinunnmaria <steinunnmaria96@gmail.com>
2022-09-04 14:26:53 +0000

    Steinunn updated git config settings. Second commit

--- src/main/java/is/hi/hbv501g13/workouttracker/WorkoutTrackerApplication.java
+++ src/main/java/is/hi/hbv501g13/workouttracker/WorkoutTrackerApplication.java
@@ -1,5 +1,5 @@
 package is.hi.hbv501g13.workouttracker;
-// This is team member Steinunn María Egilsdóttir
+// This is team member Steinunn María Egilsdóttir - updated git config settings
 // This is team member Viktoría Emma Berglindardóttir
 // This is team member Jón Jónsson
 import org.springframework.boot.SpringApplication;
