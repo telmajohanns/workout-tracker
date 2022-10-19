74226347699068bfcba523bab6ba397540aecac1
hebbi4 <hebbi4@gmail.com>
2022-10-19 14:05:02 +0000

    added some templates and almost finished with support video 5

--- src/main/java/is/hi/hbv501g13/workouttracker/Controllers/HomeController.java
+++ src/main/java/is/hi/hbv501g13/workouttracker/Controllers/HomeController.java
@@ -1,7 +1,16 @@
 package is.hi.hbv501g13.workouttracker.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+
+    @RequestMapping(value = "/")
+    public String landingPage(){
+        return "landingPage";
+    }
 }
--- src/main/java/is/hi/hbv501g13/workouttracker/Controllers/UserController.java
+++ src/main/java/is/hi/hbv501g13/workouttracker/Controllers/UserController.java
@@ -1,10 +1,15 @@
 package is.hi.hbv501g13.workouttracker.Controllers;
 
+import is.hi.hbv501g13.workouttracker.Persistance.Entities.User;
 import is.hi.hbv501g13.workouttracker.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -15,13 +20,50 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping("/")
-    public String landingPage(Model model){
-        //Business logic
-        //Call a method in a Service Class
 
-        //Add some data to the Model
-        model.addAttribute("Select user", );
-        return "landingPage";
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signupGET(User user){
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signupPOST(User user, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "redirect:/signup";
+        }
+        User exists = userService.findByUsername(user.getUsername());
+        if(exists == null){
+            userService.save(user);
+        }
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginGET(User user){
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginPOST(User user, BindingResult result, Model model, HttpSession session){
+        if(result.hasErrors()){
+            return "login";
+        }
+        User exists = userService.login(user);
+        if(exists != null){
+            session.setAttribute("LoggedInUser", exists);
+            model.addAttribute("LoggedInUser", exists);
+            return "loggedInUser";
+        }
+        return  "redirect:/";
+    }
+
+    @RequestMapping(value = "/loggedin", method = RequestMethod.GET)
+    public String loggedinGET(HttpSession session, Model model){
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        if(sessionUser != null){
+            model.addAttribute("LoggedInUser", sessionUser);
+            return "loggedInUser";
+        }
+        return "redirect:/";
     }
 }
