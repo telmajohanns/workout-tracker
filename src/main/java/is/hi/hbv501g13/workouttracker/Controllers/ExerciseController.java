bf024a6324a79299dcbfd9119fdc55847d3b786c
steinunnmaria <steinunnmaria96@gmail.com>
2022-10-25 19:08:07 +0000

    Null exercise cannot be created

--- src/main/java/is/hi/hbv501g13/workouttracker/Controllers/ExerciseController.java
+++ src/main/java/is/hi/hbv501g13/workouttracker/Controllers/ExerciseController.java
@@ -4,7 +4,7 @@ package is.hi.hbv501g13.workouttracker.Controllers;
 import is.hi.hbv501g13.workouttracker.Persistance.Entities.Exercise;
 import is.hi.hbv501g13.workouttracker.Persistance.Entities.User;
 import is.hi.hbv501g13.workouttracker.Services.Implementations.ExerciseServiceImplementation;
-import is.hi.hbv501g13.workouttracker.Services.Implementations.UserServiceImplementation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,18 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
-import java.awt.event.ActionEvent;
+
 
 
 @Controller
 public class ExerciseController {
-
     private ExerciseServiceImplementation exerciseService;
-    // Vantar að fá hvaða user er loggaður inn??
-    private Long userID;
-    private User user;
-    //private UserServiceImplementation userService;
-    //private Exercise exercise;
 
     @Autowired
     public ExerciseController(ExerciseServiceImplementation exerciseService) {
@@ -39,12 +33,12 @@ public class ExerciseController {
     @RequestMapping(value = "/exercise", method = RequestMethod.POST)
     public String exercisePOST(Exercise exercise, BindingResult result, Model model, HttpSession session){
         if(result.hasErrors()){
-            return "redirect:/exercise"; // ATH
+            return "redirect:/exercise";
         }
         User sessionUser = (User) session.getAttribute("LoggedInUser");
         if (sessionUser != null) {
             exercise.setUser(sessionUser);
-            if (!exerciseService.checkExists(exercise.getName())) {
+            if (!exerciseService.checkExists(exercise.getName()) && exercise.getName().length() != 0) {
                 exerciseService.save(exercise);
                 System.out.println("Successsss");
                 return "redirect:/exercise";
